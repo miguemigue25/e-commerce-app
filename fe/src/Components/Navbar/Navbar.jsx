27b2f61dcb7d0cd98a7b6a2da43e8ce2653f3473
Miguel <migueortiz0125@gmail.com>
2024-02-19 21:41:45 -0800

    added a few starting Pages

--- fe/src/Components/Navbar/Navbar.jsx
+++ fe/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("shop");
+
+
   return (
     <div className='navbar'>
       <div className="nav-logo">
@@ -12,10 +16,10 @@ const Navbar = () => {
         <p>SHOPPER</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop <hr /></li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={() => {setMenu("shop")}} >Shop {menu==="shop"?<h/>:<></>} </li>
+        <li onClick={() => {setMenu("mens")}} >Men {menu==="mens"?<h/>:<></>} </li>
+        <li onClick={() => {setMenu("womens")}} >Women {menu==="womens"?<h/>:<></>} </li>
+        <li onClick={() => {setMenu("kids")}} >Kids {menu==="kids"?<h/>:<></>} </li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
@@ -26,6 +30,6 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
 
-// 19:06
+// 24:44
