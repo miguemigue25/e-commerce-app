aba3858b8463bbe8e59ec8468ad0365c50cbb1d7
Jacob Delott <jldelott@gmail.com>
2024-04-02 10:51:27 -0700

    finally got mongodb atlas authenticated correctly

--- be/index.js
+++ be/index.js
@@ -10,3 +10,24 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
+// Database Connection With MongoDB
+
+mongoose.connect("mongodb+srv://jldelott:jackson085@cluster0.d0gxsqg.mongodb.net/");
+
+
+// API Creation
+
+app.get("/",(req,res)=>{
+    res.send("Experess App is Running")
+
+})
+
+app.listen(port,(error)=>{
+    if (!error) {
+        console.log("Server is running on port" +port)
+    }
+    else
+    {
+        console.log("Error : " +error)
+    }
+})
