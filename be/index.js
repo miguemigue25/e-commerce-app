5a8b5bd17a21f8c09a60957de1834079005674d5
Jacob Delott <jldelott@gmail.com>
2024-04-06 12:58:56 -0700

    added login

--- be/index.js
+++ be/index.js
@@ -181,6 +181,31 @@ app.post('/signup',async (req,res)=>{
 
 })
 
+// Creating Endpoint for Use Login
+
+app.post('/login', async (req,res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if (user) {
+        const passCompare = req.body.password === user.password;
+        if (passCompare) {
+            const data = {
+                user:{
+                    id:user.id
+                } 
+            }
+            const token = jwt.sign(data,'secret_ecom');
+            res.json({success:true,token});
+        }
+        else{
+            res.json({success:false,errors:'Wrong Password'});
+        }
+    }
+    else{
+        res.json({success:false,errors:"Wrong Email Id"})
+    }
+
+})
+
 
 
 app.listen(port,(error)=>{
