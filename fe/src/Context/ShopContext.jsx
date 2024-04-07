f629dd523f26face631a8c24260130ac45f97189
Jacob Delott <jldelott@gmail.com>
2024-04-07 15:23:13 -0700

    shopcontext

--- fe/src/Context/ShopContext.jsx
+++ fe/src/Context/ShopContext.jsx
@@ -23,9 +23,22 @@ const ShopContextProvider = (props) => {
         .then((data)=>setAll_Product(data))
     },[])
     
-    const addToCart = (itemId) => {
-        setCartItems((prev) => ({...prev, [itemId]: prev[itemId]+ 1}));
-    } 
+   const addToCart = (itemId) => {
+    setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}));
+    if (localStorage.getItem('auth-token')) {
+        fetch('http://localhost:4000/addtocart', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/form-data',
+                'auth-token': `${localStorage.getItem('auth-token')}`,
+                'Content-Type': 'application/json',
+            },
+            body:JSON.stringify({"itemId":itemId}),
+        })
+        .then((response)=>response.json())
+        .then((data)=>console.log(data))
+    }
+}
 
     const removeFromCart = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]: prev[itemId]- 1}));
